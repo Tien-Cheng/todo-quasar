c8796a00d81ac9e99bd4b22d8755f8eb4a10f4e9
Tien-Cheng <tiencheng@protonmail.com>
2022-10-19 13:18:48 +0800

    [feat] Add ability to edit and remove tasks

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,13 +1,52 @@
 <template>
-  <div>My component</div>
-  <ol>
-    <li v-for="task in tasks" :key="task.id">{{ task.content }}</li>
-  </ol>
+  <q-list bordered separator>
+    <q-item-label header>Current Tasks</q-item-label>
+    <q-item
+      clickable
+      v-ripple
+      v-for="task in tasks"
+      :key="task.id"
+      class="q-pa-md"
+      ><q-item-section>{{ task.content }}</q-item-section>
+      <q-item-section side>
+        <div class="q-gutter-sm">
+          <q-btn
+            round
+            icon="edit"
+            @click="callEdit(task.content, task.id)"
+          ></q-btn>
+          <q-btn
+            round
+            color="negative"
+            icon="delete"
+            @click="$emit('deleteTask', task.id)"
+          ></q-btn>
+        </div>
+      </q-item-section>
+    </q-item>
+  </q-list>
+  <q-dialog v-model="edit">
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">Edit Task</div>
+        <q-form @submit="onEdit">
+          <q-input v-model="currentTask.content" label="Task"></q-input>
+          <q-btn
+            type="submit"
+            label="Edit Task"
+            color="primary"
+            v-close-popup
+          ></q-btn>
+        </q-form>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, defineEmits, PropType, reactive, ref } from 'vue';
 import { Todo } from './models';
+
 export default defineComponent({
   name: 'TodoList',
   props: {
@@ -15,6 +54,29 @@ export default defineComponent({
       type: Array as PropType<Todo[]>,
       default: () => [],
     },
+  },
+  emits: ['editTask'],
+  setup(props, ctx) {
+    const edit = ref(false);
+    const currentTask = reactive({} as Todo);
+
+    function callEdit(content: string, id: number) {
+      currentTask.content = content;
+      currentTask.id = id;
+      edit.value = true;
+    }
+
+    function onEdit() {
+      if (currentTask.content) {
+        ctx.emit('editTask', currentTask);
+      }
+    }
+    return {
+      edit,
+      currentTask,
+      callEdit,
+      onEdit,
+    };
   },
 });
 </script>
--- src/pages/TodosPage.vue
+++ src/pages/TodosPage.vue
@@ -1,44 +1,80 @@
+<style></style>
 <template>
   <q-page padding>
     <!-- content -->
-    <h3>Your Todos</h3>
-    <q-form
-      @submit="onSubmit"
-    >
-      <q-input filled v-model="task" label="Task" hint="Task" lazy-rules :rules="[ val => val && val.length > 0 || 'Please type something']"/>
-      <q-btn label="Submit" type="submit" color="primary"></q-btn>
+    <h3 class="tw-underline">Your Todos</h3>
+    <q-form @submit="onSubmit" ref="form">
+      <div class="row">
+        <div class="col">
+          <q-input
+            class="col"
+            filled
+            v-model="task"
+            label="Task"
+            hint="Task"
+            lazy-rules
+            :rules="[
+              (val) => (val && val.length > 0) || 'Please type something',
+            ]"
+          />
+        </div>
+        <div class="col q-pa-sm">
+          <q-btn round type="submit" color="primary" icon="add"></q-btn>
+        </div>
+      </div>
     </q-form>
-    <todo-list :tasks="tasks"
+    <todo-list
+      :tasks="tasks"
+      class="q-mt-md"
+      @delete-task="deleteTask"
+      @edit-task="editTask"
     ></todo-list>
   </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, reactive } from 'vue';
+import { defineComponent, ref, reactive, Ref } from 'vue';
 import { Todo } from 'src/components/models';
 import TodoList from 'src/components/TodoList.vue';
+import { QForm } from 'quasar';
+
 export default defineComponent({
   // name: 'PageName'
   components: {
-    TodoList
+    TodoList,
   },
-  setup () {
-    let id = 1;
-    const task = ref(null)
+  setup() {
+    let id = 0;
+    const task: Ref<string | null >= ref(null);
     const tasks: Todo[] = reactive([]);
+    const form = ref<QForm>();
 
     return {
       task,
       tasks,
+      form,
       onSubmit() {
         if (task.value) {
           tasks.push({
-            id: ++id,
-            content: task.value
+            id: id++,
+            content: task.value,
           });
+          task.value = null;
+          form.value?.resetValidation();
         }
+      },
+      deleteTask(id: number) {
+        tasks.splice(tasks.findIndex(
+          (todo) => todo.id === id
+        ), 1)
+      },
+      editTask(task: Todo) {
+        let idx = tasks.findIndex(
+          (t) => t.id === task.id
+        )
+        tasks[idx].content = task.content
       }
-    }
-  }
-})
+    };
+  },
+});
 </script>
